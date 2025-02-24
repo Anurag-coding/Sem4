6007a13a5acc2c945dd2c41865c14d97829ca610
Anurag kumar <153097845+Anurag-coding@users.noreply.github.com>
2025-02-24 10:33:23 +0530

    Update car.java

--- car.java
+++ car.java
@@ -12,11 +12,11 @@ class text{
 }
 public class car {
 	public static void main(String[] args) {
-//		//car car1=new car("farari","xyz");
-//		//car car2 = new car(null,null);
-//		//System.out.println("make and model for car-1 : "+car1.toString());
-//		System.out.println("make and model for car-2 : "+car2.toString());
-//		//car2.setter("bmw", "abc");
-//		System.out.println("make and model for car-2 : "+car2.toString());
+		car car1=new car("farari","xyz");
+		car car2 = new car(null,null);
+		System.out.println("make and model for car-1 : "+car1.toString());
+		System.out.println("make and model for car-2 : "+car2.toString());
+		car2.setter("bmw", "abc");
+		System.out.println("make and model for car-2 : "+car2.toString());
 	}
 }
