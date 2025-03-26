667bdc4e6416af3055aed2ced8a3eedd889ebee8
Anurag kumar <153097845+Anurag-coding@users.noreply.github.com>
2025-03-26 15:08:05 +0530

    Add files via upload

---
+++ cleaner.java
@@ -0,0 +1,39 @@
+import java.lang.ref.Cleaner;
+
+import Student.CleanupTask;
+class Student{
+	private String name;
+	private int rollno;
+	private static final Cleaner cleaner = Cleaner.create();
+	private final Cleaner.Cleanable cleanable;
+	public Student(String name,int rollno) {
+		this.name=name;
+		this.rollno=rollno;
+		this.cleanable=cleaner.register(this,new CleanupTask(name));
+	}
+	public void display() {
+		System.out.println("Name: "+name+"\n"+"Rollno: "+rollno);
+	}
+	private static class CleanupTask implements Runnable{
+		private final String name;
+		CleanupTask(String name){
+			this.name=name;
+		}
+		@Override
+		public void run() {
+			System.out.println("Cleanup: "+name);
+		}
+    }
+}
+public class cleaner {
+	public static void main(String[] args) {
+		Student s1 = new Student("Anurag",198);
+		Student s2 = new Student("Sohan",201);
+		s1.display();
+		s2.display();
+		s1=null;
+		s2=null;
+		System.gc();
+	}
+}
+
